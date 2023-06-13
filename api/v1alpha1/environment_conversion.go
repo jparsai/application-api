f58fa519d61718113f196e84f55f4bc4745c19b5
jparsai <jparsai@redhat.com>
2023-06-13 16:06:04 +0530

    Create new Environment API version.

---
+++ api/v1alpha1/environment_conversion.go
@@ -0,0 +1,44 @@
+package v1alpha1
+
+import (
+	"fmt"
+
+	"github.com/redhat-appstudio/application-api/api/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/conversion"
+)
+
+// ConvertTo converts this Memcached to the Hub version (vbeta1).
+func (src *Environment) ConvertTo(dstRaw conversion.Hub) error {
+
+	fmt.Println("1111 $$$$$$$$$$$$$$$$$$")
+	fmt.Println("1111 $$$$$$$$$$$$$$$$$$")
+
+	dst := dstRaw.(*v1beta1.Environment)
+
+	dst.ObjectMeta = src.ObjectMeta
+
+	dst.Spec.DisplayName = "DisplayName111"
+	dst.Spec.DeploymentStrategy = v1beta1.DeploymentStrategyType(src.Spec.DeploymentStrategy)
+	dst.Spec.ParentEnvironment = "ParentEnvironment111"
+	dst.Spec.Tags = src.Spec.Tags
+
+	return nil
+}
+
+// ConvertFrom converts from the Hub version (vbeta1) to this version.
+func (dst *Environment) ConvertFrom(srcRaw conversion.Hub) error {
+
+	fmt.Println("2222 $$$$$$$$$$$$$$$$$$")
+	fmt.Println("2222 $$$$$$$$$$$$$$$$$$")
+
+	src := srcRaw.(*v1beta1.Environment)
+
+	dst.ObjectMeta = src.ObjectMeta
+
+	dst.Spec.DisplayName = "DisplayName222"
+	dst.Spec.DeploymentStrategy = DeploymentStrategyType(src.Spec.DeploymentStrategy)
+	dst.Spec.ParentEnvironment = "ParentEnvironment222"
+	dst.Spec.Tags = src.Spec.Tags
+
+	return nil
+}
