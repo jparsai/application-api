0c6e1a6866c2133b2df2f311bcc05b09bc2569d8
jparsai <jparsai@redhat.com>
2023-07-18 17:50:10 +0530

    Create new Environment API version.

--- api/v1beta1/environment_webhook.go
+++ api/v1beta1/environment_webhook.go
@@ -14,17 +14,22 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1alpha1
+package v1beta1
 
 import (
 	"fmt"
 
-	"k8s.io/apimachinery/pkg/runtime"
+	runtime "k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+const (
+	MissingIngressDomain    = "ingress domain cannot be empty if cluster is of type Kubernetes"
+	InvalidDNS1123Subdomain = "invalid ingress domain: %q: an ingress domain must consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character"
 )
 
 // log is for logging in this package.
@@ -36,26 +41,25 @@ func (r *Environment) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
-//+kubebuilder:webhook:path=/mutate-appstudio-redhat-com-v1alpha1-environment,mutating=true,failurePolicy=fail,sideEffects=None,groups=appstudio.redhat.com,resources=environments,verbs=create;update,versions=v1alpha1,name=menvironment.kb.io,admissionReviewVersions={v1,v1beta1}
+//+kubebuilder:webhook:path=/mutate-appstudio-redhat-com-v1beta1-environment,mutating=true,failurePolicy=fail,sideEffects=None,groups=appstudio.redhat.com,resources=environments,verbs=create;update,versions=v1beta1,name=menvironment.kb.io,admissionReviewVersions={v1,v1beta1}
 
 var _ webhook.Defaulter = &Environment{}
 
 // Default implements webhook.Defaulter so a webhook will be registered for the type
 func (r *Environment) Default() {
-
-	// TODO(user): fill in your defaulting logic.
+	environmentlog.Info("default", "name", r.Name)
 }
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
-//+kubebuilder:webhook:path=/validate-appstudio-redhat-com-v1alpha1-environment,mutating=false,failurePolicy=fail,sideEffects=None,groups=appstudio.redhat.com,resources=environments,verbs=create;update,versions=v1alpha1,name=venvironment.kb.io,admissionReviewVersions={v1,v1beta1}
+//+kubebuilder:webhook:path=/validate-appstudio-redhat-com-v1beta1-environment,mutating=false,failurePolicy=fail,sideEffects=None,groups=appstudio.redhat.com,resources=environments,verbs=create;update,versions=v1beta1,name=venvironment.kb.io,admissionReviewVersions={v1,v1beta1}
 
 var _ webhook.Validator = &Environment{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *Environment) ValidateCreate() (admission.Warnings, error) {
-	environmentlog := environmentlog.WithValues("controllerKind", "Environment").WithValues("name", r.Name).WithValues("namespace", r.Namespace)
+	environmentlog := environmentlog.WithValues("controllerKind", "Environment").
+		WithValues("name", r.Name).WithValues("namespace", r.Namespace)
+
 	environmentlog.Info("validating the create request")
 
 	// We use the DNS-1123 format for environment names, so ensure it conforms to that specification
@@ -68,22 +72,20 @@ func (r *Environment) ValidateCreate() (admission.Warnings, error) {
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *Environment) ValidateUpdate(old runtime.Object) (admission.Warnings, error) {
-	environmentlog := environmentlog.WithValues("controllerKind", "Environment").WithValues("name", r.Name).WithValues("namespace", r.Namespace)
+	environmentlog := environmentlog.WithValues("controllerKind", "Environment").
+		WithValues("name", r.Name).WithValues("namespace", r.Namespace)
+
 	environmentlog.Info("validating the update request")
 
 	return nil, r.validateIngressDomain()
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
-func (r *Environment) ValidateDelete() (admission.Warnings, error) {
-
-	// TODO(user): fill in your validation logic upon object deletion.
-	return nil, nil
-}
+func (r *Environment) ValidateDelete() (admission.Warnings, error) { return nil, nil }
 
 // validateIngressDomain validates the ingress domain
 func (r *Environment) validateIngressDomain() error {
-	unstableConfig := r.Spec.UnstableConfigurationFields
+	unstableConfig := r.Spec.Target
 	if unstableConfig != nil {
 		// if cluster type is Kubernetes, then Ingress Domain should be set
 		if unstableConfig.ClusterType == ConfigurationClusterType_Kubernetes && unstableConfig.IngressDomain == "" {
@@ -94,8 +96,6 @@ func (r *Environment) validateIngressDomain() error {
 		if unstableConfig.IngressDomain != "" && len(validation.IsDNS1123Subdomain(unstableConfig.IngressDomain)) != 0 {
 			return fmt.Errorf(InvalidDNS1123Subdomain, unstableConfig.IngressDomain)
 		}
-
 	}
-
 	return nil
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	k8s.io/api v0.27.2
 	k8s.io/apiextensions-apiserver v0.27.2
 	k8s.io/apimachinery v0.27.2
+	k8s.io/client-go v0.27.2
 	sigs.k8s.io/controller-runtime v0.15.0
 
 )
@@ -64,7 +65,6 @@ require (
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/client-go v0.27.2 // indirect
 	k8s.io/component-base v0.27.2 // indirect
 	k8s.io/klog/v2 v2.90.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230501164219-8b0f38b5fd1f // indirect
