0921df57954093e3d2a3462273a487a23dad1a80
jparsai <jparsai@redhat.com>
2023-09-29 15:39:41 +0530

    Create new Environment API version.

--- api/v1alpha1/environment_conversion.go
+++ api/v1alpha1/environment_conversion.go
@@ -16,12 +16,18 @@
 package v1alpha1
 
 import (
+	"fmt"
+
 	"github.com/redhat-appstudio/application-api/api/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 )
 
 // ConvertTo converts this Memcached to the Hub version (vbeta1).
 func (src *Environment) ConvertTo(dstRaw conversion.Hub) error {
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 
 	// fetch v1beta1 version from Hub, converted values will be set in this object
 	dst := dstRaw.(*v1beta1.Environment)
@@ -37,6 +43,8 @@ func (src *Environment) ConvertTo(dstRaw conversion.Hub) error {
 		Tags:               src.Spec.Tags,
 	}
 
+	fmt.Println("111.  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+
 	// if v1alpha1 version has src.Spec.Configuration.Env field then copy it to v1beta1
 	if src.Spec.Configuration.Env != nil {
 		dst.Spec.Configuration.Env = []v1beta1.EnvVarPair{}
@@ -45,9 +53,13 @@ func (src *Environment) ConvertTo(dstRaw conversion.Hub) error {
 			dst.Spec.Configuration.Env = append(dst.Spec.Configuration.Env, v1beta1.EnvVarPair(env))
 		}
 	}
+	fmt.Println("222.  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+
+	fmt.Println("00. ####### src.Spec.Configuration.Target.DeploymentTargetClaim.ClaimName === ", src.Spec.Configuration.Target.DeploymentTargetClaim.ClaimName)
 
 	// if v1alpha1 version has Spec.Configuration.Target field then copy it to v1beta1
 	if src.Spec.Configuration.Target.DeploymentTargetClaim.ClaimName != "" {
+		fmt.Println("00. ##### inside condition")
 		// This filed is renamed and moved to Target in v1beta1
 		dst.Spec.Target = &v1beta1.TargetConfiguration{
 			Claim: v1beta1.TargetClaim{
@@ -59,13 +71,22 @@ func (src *Environment) ConvertTo(dstRaw conversion.Hub) error {
 	}
 
 	// if v1alpha1 has Spec.UnstableConfigurationFields field then copy it to v1beta1
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	fmt.Println("11. ####### src.Spec.UnstableConfigurationFields === ", src.Spec.UnstableConfigurationFields)
 	if src.Spec.UnstableConfigurationFields != nil {
 
+		fmt.Println("22. ####### inside condition")
+		fmt.Println("33. ####### dst.Spec.Target === ", dst.Spec.Target)
 		if dst.Spec.Target == nil {
+			fmt.Println("44. ##### inside condition")
 			dst.Spec.Target = &v1beta1.TargetConfiguration{}
 		}
 
+		fmt.Println("55. ##### src.Spec.UnstableConfigurationFields.ClusterType == ", src.Spec.UnstableConfigurationFields.ClusterType)
+
 		dst.Spec.Target.ClusterType = v1beta1.ConfigurationClusterType(string(src.Spec.UnstableConfigurationFields.ClusterType))
+
+		fmt.Println("66. ##### src.Spec.UnstableConfigurationFields.KubernetesClusterCredentials.TargetNamespace == ", src.Spec.UnstableConfigurationFields.KubernetesClusterCredentials.TargetNamespace)
 
 		dst.Spec.Target.KubernetesClusterCredentials = v1beta1.KubernetesClusterCredentials{
 			TargetNamespace:            src.Spec.UnstableConfigurationFields.KubernetesClusterCredentials.TargetNamespace,
